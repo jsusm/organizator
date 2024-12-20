efb0a897bdcd5ee6abf2aa2bda914d490fd0cddb
jsusm <jesusjaviermarcanomora.2005@gmail.com>
2024-12-20 13:38:33 -0400

    install area chart component, render balance data

---
+++ components/dashboard/balance-chart.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { balancePerDay } from '~/mockData/operations';
+
+const chartData = balancePerDay.map((info) => ({ balance: info.balance, date: new Intl.DateTimeFormat("en-US", { month: 'short', day: "numeric" }).format(info.date) }))
+
+const xFormatter = (tick: number | Date, i: number, ticks: number[] | Date[]) => {
+  return `\$${chartData[i].balance}`
+}
+
+</script>
+<template>
+  <AreaChart class="h-48 w-96" :data="chartData" index="date" :y-formatter="xFormatter" :categories="['balance']"
+    :colors="['#fbbf24']" />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <div class="flex justify-center py-64">
-    <DashboardTransactionsCalendarChart />
+  <div class="flex justify-center py-32">
+    <DashboardBalanceChart/>
   </div>
 </template>
