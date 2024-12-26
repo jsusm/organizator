d698d5a519b0f78e4ba41c1b90ec4ff20807ed34
jsusm <jesusjaviermarcanomora.2005@gmail.com>
2024-12-26 16:41:57 -0400

    Create operation to category relation, uses schema

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 export const categories = sqliteTable(
@@ -22,3 +23,10 @@ export const operations = sqliteTable(
   },
   () => [],
 );
+
+export const operationsRelations = relations(operations, ({one}) => ({
+  category: one(categories, {
+    fields: [operations.categoryId],
+    references: [categories.id],
+  })
+}))
--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,6 +1,10 @@
+import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
+import * as schema from '@/server/database/schema'
 
-export const db = drizzle({ connection: { url: process.env.DB_FILE_NAME! } });
+const client = createClient({ url: process.env.DB_FILE_NAME! })
+
+export const db = drizzle(client, { schema });
 
 export function useDB() {
   return db;
