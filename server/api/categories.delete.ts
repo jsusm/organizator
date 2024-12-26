0777c25e74e877fd7fa93b58a47af5113d2fe7b4
jsusm <jesusjaviermarcanomora.2005@gmail.com>
2024-12-25 20:35:33 -0400

    Create server route for delete categories

---
+++ server/api/categories.delete.ts
@@ -0,0 +1,19 @@
+import * as z from "zod";
+import { categories } from "../database/schema";
+import { eq } from "drizzle-orm";
+
+export default defineEventHandler(async (event) => {
+  // TODO: handle authentication
+  const query = await getValidatedQuery(
+    event,
+    z.object({
+      id: z.number({ coerce: true }).int(),
+    }).parse,
+  );
+
+  const db = useDB();
+
+  await db.delete(categories).where(eq(categories.id, query.id));
+
+  return { success: true };
+});
